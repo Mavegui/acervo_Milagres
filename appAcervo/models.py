732dde9d8890df7c52a682c3cd0280b6c3a43de4
mavegui <lord77xgui@gmail.com>
2026-07-13 11:53:55 -0300

    Using Trim

--- appAcervo/models.py
+++ appAcervo/models.py
@@ -6,9 +6,9 @@ def renomear_arquivo(instance, filename):
     extensao = filename.split('.')[-1]
     novo_nome = f"{slugify(instance.titulo)}.{extensao}"
     return novo_nome
-           
+
 class Foto(models.Model):
-    
+
     id = models.AutoField(primary_key=True)
     titulo = models.CharField(
         max_length=200,
@@ -19,7 +19,7 @@ class Foto(models.Model):
         max_length=400,
         verbose_name="Descrição/História"
     )
-    
+
     CATEGORIAS_CHOICES = [
         ('REL', 'Religiosidade'),
         ('ARQ', 'Arquitetura e Urbanismo'),
@@ -30,13 +30,13 @@ class Foto(models.Model):
         ('SIM', 'Símbolos'),
         ('MON', 'Monumentos'),
     ]
-    
+
     categoria = models.CharField(
         max_length=3,
         choices=CATEGORIAS_CHOICES,
         verbose_name="Categoria"
     )
-    
+
     data_estimada = models.DateField(
         null=True,
         blank=True,
@@ -47,19 +47,19 @@ class Foto(models.Model):
         verbose_name="Cedido por / Fonte",
         help_text="Nome da pessoa ou orgão que disponibilizou a imagem."
     )
-    
+
     imagem = models.ImageField(
         upload_to=renomear_arquivo,
         verbose_name="Arquivo da foto",
         max_length=500
     )
-    
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
         return f"{self.titulo} [{self.get_categoria_display()}]"
-    
-    class Meta: 
+
+    class Meta:
         verbose_name = "Foto"
         verbose_name_plural = "Fotos"
